c684ebc7e80438715c150da7c55101abd5d9a89c
divaldohelder08 <divaldohelder08@gmail.com>
2023-10-09 19:44:39 +0100

    fixe: remove image bug

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
     <Container>
       <div className="space-y-10 pb-10">
         <div className="p-4 sm:p-6 lg:p-8 rounded-lg overflow-hidden">
-          <div style={{ backgroundImage: `url(/src/assets/forest.jpg)` }}
+          <div style={{ backgroundImage: `url(/public/assets/forest.jpg)` }}
             className="rounded-lg relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
             <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
               <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs text-black dark:text-white bg-secondary/60 p-4 rounded-lg">
